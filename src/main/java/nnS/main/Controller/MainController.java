d608a70217b4c3e6270c42204d6f7bf962ae43ab
yongyeonKim <>
2020-01-28 14:20:18 +0900

    c => C

--- src/main/java/nnS/main/Controller/MainController.java
+++ src/main/java/nnS/main/Controller/MainController.java
@@ -1,4 +1,4 @@
-package nnS.main.controller;
+package nnS.main.Controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -13,6 +13,7 @@ public class MainController {
 	@RequestMapping(value="/main")
     public ModelAndView main(CommandMap commandMap) throws Exception{
     	ModelAndView mv = new ModelAndView("/member/main/main");
+    	
     	return mv; 
     }
 }
